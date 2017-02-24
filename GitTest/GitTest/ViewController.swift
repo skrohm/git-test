170b83d1381efa11d1affa1e8908d3946704ca10
Sascha Krohm <sascha@krohm.de>
2017-02-24 23:14:39 +0100

    Initialer commit

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Sascha Krohm on 24.02.17.
+//  Copyright © 2017 Sascha Krohm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
